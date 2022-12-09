4766c46493e4c7bed23c4bb6ee64295bc7cc2858
Jean-Luc Mounsamy <jl.mounsamy@gmail.com>
2022-12-09 18:56:39 -0500

    Configure Jest code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,20 @@
 module.exports = {
   roots: [
-    '<rootDir>',
+    '<rootDir>'
   ],
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
   ],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest'
   },
-};
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
+  coverageThreshold: {
+    global: {
+      lines: 90
+    }
+  }
+}
